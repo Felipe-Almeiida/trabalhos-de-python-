c07cd5cbbfeea4c29fb7976b865319185347a175
Felipe Almeida <161991195+Felipe-Almeiida@users.noreply.github.com>
2024-03-05 11:09:19 -0300

    Add files via upload

---
+++ perimetro_de_circunferencia.py
@@ -0,0 +1,5 @@
+from math import pi
+# programa que calcula perimetro de uma circunferencia 
+raio = float(input('informe o valor do raio: '))
+perimetro = 2 * pi * raio 
+print(f'o valor do perimetro é: {perimetro}')
