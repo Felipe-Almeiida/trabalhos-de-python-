359cd81892ba4e023a5292da83e464cf0ae41ded
Felipe Almeida <161991195+Felipe-Almeiida@users.noreply.github.com>
2024-03-12 09:41:21 -0300

    par_impar
    informe se o numero que foi digitado é impar ou par

---
+++ par_impar.py
@@ -0,0 +1,11 @@
+''''
+Informa se um numero é par ou impár 
+
+'''
+
+numero = int(input('digite algum numero: '))
+
+if numero % 2 == 0:
+    print(f'numero é par')
+else:
+    print(f'numero é impar')
